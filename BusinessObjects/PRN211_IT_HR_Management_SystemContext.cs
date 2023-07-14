82deb02c3a1fbd00998a32bd57507852e61a038a
HuyTran0211 <quochuy2112002@gmail.com>
2023-07-14 19:46:24 +0700

    Huy

--- BusinessObjects/PRN211_IT_HR_Management_SystemContext.cs
+++ BusinessObjects/PRN211_IT_HR_Management_SystemContext.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.IO;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
 
 #nullable disable
 
@@ -24,14 +26,23 @@ namespace BusinessObjects
         public virtual DbSet<PersonalContract> PersonalContracts { get; set; }
         public virtual DbSet<TakeLeave> TakeLeaves { get; set; }
         public virtual DbSet<Tax> Taxes { get; set; }
+        private string GetConnectionString()
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", true, true).Build();
+            return configuration["ConnectionStrings:DefaultConnectionString"];
+        }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=(local);uid=sa;pwd=12345678;database=PRN211_IT_HR_Management_System;TrustServerCertificate=True;");
-            }
+            //if (!optionsBuilder.IsConfigured)
+            //{
+            //    //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+            //    //                optionsBuilder.UseSqlServer("Server=(local);uid=sa;pwd=12345678;database=PRN211_IT_HR_Management_System;TrustServerCertificate=True;");
+               
+            //}
+            optionsBuilder.UseSqlServer(GetConnectionString());
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
