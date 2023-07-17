34a4dedd2b2bf47ce2e1d753f1d9c6bae16acf56
HuyTran0211 <quochuy2112002@gmail.com>
2023-07-17 07:00:36 +0700

    Huy
    Staff

--- BusinessObjects/PRN211_IT_HR_Management_SystemContext.cs
+++ BusinessObjects/PRN211_IT_HR_Management_SystemContext.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.IO;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
 
 #nullable disable
 
@@ -17,26 +19,53 @@ namespace BusinessObjects
         {
         }
 
+        public virtual DbSet<Attendance> Attendances { get; set; }
         public virtual DbSet<Candidate> Candidates { get; set; }
         public virtual DbSet<Employee> Employees { get; set; }
         public virtual DbSet<Overtime> Overtimes { get; set; }
         public virtual DbSet<Payslip> Payslips { get; set; }
-        public virtual DbSet<PersonalContract> PersonalContracts { get; set; }
         public virtual DbSet<TakeLeave> TakeLeaves { get; set; }
-        public virtual DbSet<Tax> Taxes { get; set; }
-
+        public virtual DbSet<TakeLeaveCount> TakeLeaveCounts { get; set; }
+        private string GetConnectionString()
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", true, true).Build();
+            return configuration["ConnectionStrings:DefaultConnectionString"];
+        }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=(local);uid=sa;pwd=1234567890;database=PRN211_IT_HR_Management_System;TrustServerCertificate=True");
-            }
+            //            if (!optionsBuilder.IsConfigured)
+            //            {
+            //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+            //                optionsBuilder.UseSqlServer("server=LAPTOP-0URV4SIH\\HUYTRAN; Database=PRN211_IT_HR_Management_System; Uid=sa; Pwd=12345");
+            //            }
+            optionsBuilder.UseSqlServer(GetConnectionString());
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Attendance>(entity =>
+            {
+                entity.ToTable("Attendance");
+
+                entity.Property(e => e.AttendanceId)
+                    .ValueGeneratedNever()
+                    .HasColumnName("AttendanceID");
+
+                entity.Property(e => e.Date).HasColumnType("datetime");
+
+                entity.Property(e => e.EmployeeId).HasColumnName("EmployeeID");
+
+                entity.Property(e => e.IsAttend).HasColumnName("isAttend");
+
+                entity.HasOne(d => d.Employee)
+                    .WithMany(p => p.Attendances)
+                    .HasForeignKey(d => d.EmployeeId)
+                    .HasConstraintName("FK_Attendance_Employee");
+            });
 
             modelBuilder.Entity<Candidate>(entity =>
             {
@@ -122,26 +151,6 @@ namespace BusinessObjects
                     .HasConstraintName("FK_Payslip_Employee");
             });
 
-            modelBuilder.Entity<PersonalContract>(entity =>
-            {
-                entity.ToTable("PersonalContract");
-
-                entity.Property(e => e.Id)
-                    .ValueGeneratedNever()
-                    .HasColumnName("ID");
-
-                entity.Property(e => e.EmployeeId).HasColumnName("EmployeeID");
-
-                entity.Property(e => e.EndDate).HasColumnType("date");
-
-                entity.Property(e => e.StartDate).HasColumnType("date");
-
-                entity.HasOne(d => d.Employee)
-                    .WithMany(p => p.PersonalContracts)
-                    .HasForeignKey(d => d.EmployeeId)
-                    .HasConstraintName("FK_PersonalContract_Employee");
-            });
-
             modelBuilder.Entity<TakeLeave>(entity =>
             {
                 entity.ToTable("TakeLeave");
@@ -158,6 +167,8 @@ namespace BusinessObjects
 
                 entity.Property(e => e.EndDate).HasColumnType("date");
 
+                entity.Property(e => e.IsAccept).HasColumnName("isAccept");
+
                 entity.Property(e => e.StartDate).HasColumnType("date");
 
                 entity.HasOne(d => d.Employee)
@@ -166,13 +177,20 @@ namespace BusinessObjects
                     .HasConstraintName("FK_TakeLeave_Employee");
             });
 
-            modelBuilder.Entity<Tax>(entity =>
+            modelBuilder.Entity<TakeLeaveCount>(entity =>
             {
-                entity.ToTable("Tax");
+                entity.ToTable("TakeLeaveCount");
 
                 entity.Property(e => e.Id)
                     .ValueGeneratedNever()
                     .HasColumnName("ID");
+
+                entity.Property(e => e.EmployeeId).HasColumnName("EmployeeID");
+
+                entity.HasOne(d => d.Employee)
+                    .WithMany(p => p.TakeLeaveCounts)
+                    .HasForeignKey(d => d.EmployeeId)
+                    .HasConstraintName("FK_TakeLeaveCount_Employee");
             });
 
             OnModelCreatingPartial(modelBuilder);
--- IT_Human_resource_manager_system/frmStaff.cs
+++ IT_Human_resource_manager_system/frmStaff.cs
@@ -1,4 +1,5 @@
 ﻿using BusinessObjects;
+using Repositories;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -14,9 +15,110 @@ namespace IT_Human_resource_manager_system
     public partial class frmStaff : Form
     {
         public Employee user;
+        IEmployeeRepo employeeRepo = new EmployeeRepo();
+        IOvertimeRepo overtime = new OvertimeRepo();
+        ITakeLeaveRepo takeLeaveRepo = new TakeLeaveRepo();
         public frmStaff()
         {
             InitializeComponent();
+        }
+
+        private void frmStaff_Load(object sender, EventArgs e)
+        {
+            LoadPerInfo();
+        }
+
+        public void LoadPerInfo()
+        {
+            try
+            {
+                var Employee = employeeRepo.GetEmployeeById(user.Id);
+                txtName.Text = Employee.Name;
+                txtPhonenumber.Text = Employee.PhoneNumber.ToString();
+                TxtEmail.Text = Employee.Email;
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        private void btnChangePass_Click(object sender, EventArgs e)
+        {
+            frmChangePass frmChangePass = new frmChangePass();
+            frmChangePass.user = user;
+            frmChangePass.ShowDialog();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if (txtTimeOT.Text == "")
+            {
+                MessageBox.Show("Enter all box before submit");
+            }
+            else
+            {
+                DateTime DateOT = dtpOT.Value;
+
+                if (int.TryParse(txtTimeOT.Text, out int time))
+                {
+                    var o = new Overtime
+                    {
+                        EmployeeId = user.Id,
+                        Date = DateOT,
+                        Time = time,
+                    };
+                    overtime.addOverTime(o);
+                    MessageBox.Show("Submit OT success");
+                }
+                else
+                {
+                    MessageBox.Show("Invalid input for Time. Please enter a valid number.");
+                }
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtTimeOT.Clear();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtPhonenumber.Text, out int phonenumber))
+            {
+                var info = new Employee
+                {
+                    Id = user.Id,
+                    Username = user.Name,
+                    Password = user.Password,
+                    Name = txtName.Text,
+                    RoleName = user.RoleName,
+                    Status = user.Status,
+                    Email = TxtEmail.Text,
+                    PhoneNumber = phonenumber,
+                };
+                employeeRepo.UpdateEmployee(info);
+                MessageBox.Show("Update Personal Information success");
+            }
+
+        }
+
+        private void btnSubmitTakeLeave_Click(object sender, EventArgs e)
+        {
+            DateTime fromdate = dtpTLFromDate.Value;
+            DateTime todate = dtpTLToDate.Value;
+            var tl = new TakeLeave
+            {
+                EmployeeId = user.Id,
+                StartDate = fromdate,
+                EndDate = todate,
+                IsAccept = false,
+                Description = rtbReasonTL.Text,
+            };
+            takeLeaveRepo.AddTakeLeave(tl);
+            MessageBox.Show("Submit takeleave form success");
         }
     }
 }
