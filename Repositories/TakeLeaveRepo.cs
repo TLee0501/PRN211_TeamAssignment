0b0aa00c08d362df237eb91e998aced838eabefc
HuyTran0211 <quochuy2112002@gmail.com>
2023-07-19 04:27:03 +0700

    huy

--- Repositories/TakeLeaveRepo.cs
+++ Repositories/TakeLeaveRepo.cs
@@ -12,9 +12,9 @@ namespace Repositories
     public class TakeLeaveRepo : ITakeleaveRepo
     {
 
-        public void Delete(TakeLeave takeLeave) => TakeLeaveDAO.Instance.Delete(takeLeave);
-        public void AddTakeLeave(TakeLeave takeLeave) => TakeLeaveDAO.Instance.addTakeLeave(takeLeave);
-        public IEnumerable<TakeLeave> GetTakeLeaves() => TakeLeaveDAO.Instance.GetTakeLeaves();
+        
+        
+        
 
         public int CountTakeLeaveInMonth(int id) => TakeLeaveDAO.Instance.CountTakeLeaveInMonth(id);
 
